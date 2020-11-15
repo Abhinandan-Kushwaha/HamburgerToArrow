400f0240a9b9604f27fae590ea137c344adfbafd
Abhinandan-Kushwaha <kushabhi5@gmail.com>
2020-11-16 01:15:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import BurgerToArrow from './burger-to-arrow';
+import { useState } from 'react';
 
 function App() {
+  const [state, setState] = useState({
+    sideBarVisible: false
+  })
+  const toggleSideBar = () => {
+    setState({ ...state, sideBarVisible: !state.sideBarVisible })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BurgerToArrow id={1}
+        tintColor="yellow"
+        visible={state.sideBarVisible}
+        onPress={toggleSideBar}
+        size="medium"
+        speed={4}
+        thickness={3} />
     </div>
   );
 }
